0e7e9ebceb865c814485c746a4b84e0503c817ea
gray <gray.wen@outlook.com>
2024-08-07 09:24:26 +0800

    add gd music search

---
+++ nextjs/app/api/gd/search/route.ts
@@ -0,0 +1,16 @@
+import { getMusicList } from '@/service/music';
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const name = searchParams.get('name');
+  const source = (searchParams.get('source') || 'tencent') as Source;
+  const pages = +(searchParams.get('pages') || 1);
+  const count = +(searchParams.get('count') || 20);
+
+  if (!name) {
+    throw new Error('name is required');
+  }
+
+  const data = await getMusicList({ name, source, count, pages });
+  return new Response(JSON.stringify(data));
+}
--- nextjs/app/api/music/route.ts
+++ nextjs/app/api/music/route.ts
@@ -1,15 +1,44 @@
-import { getMusicInfo } from '@/service/music';
+import prisma from '@/prisma/prisma';
+import { Prisma } from '@prisma/client';
+
+interface ISearchResult {
+  id: number;
+  name: string;
+  singer: string;
+  album: string;
+  coverId: number;
+  lyric: string;
+}
+
+const search = async (id: number) => {
+  return await prisma.$queryRaw<ISearchResult[]>(
+    Prisma.sql`
+        SELECT
+        	M."id",
+        	M."name" AS "name",
+        	S."name" AS singer,
+        	AL."name" AS album,
+        	AL."coverId" as "coverId",
+        	L.text as lyric
+        FROM
+        	"Music" M 
+          LEFT JOIN "MusicArtist" MA ON M.ID = MA."musicId"
+        	LEFT JOIN "Singer" S ON ma."singerId" = S."id"
+        	LEFT JOIN "Album" AL ON M."albumId" = AL."id"
+        	LEFT JOIN "Lyric" L ON L."id" = M."lyricId"
+        WHERE
+        	M."id" = ${Prisma.join([id])};`
+  );
+};
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const id = searchParams.get('id');
-  const source = (searchParams.get('source') || 'tencent') as Source;
-  const br = +(searchParams.get('br') || 320) as MusicBr;
+  const id = +(searchParams.get('id') || 0);
 
   if (!id) {
     throw new Error('id is required');
   }
 
-  const data = await getMusicInfo({ id, source, br });
+  const data = await search(id);
   return new Response(JSON.stringify(data));
 }
