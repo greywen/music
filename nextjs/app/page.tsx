c8a3e31fc9689f131f8b5d250471245d57b37a3b
gray <gray.wen@outlook.com>
2024-08-08 16:34:25 +0800

    add express

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -9,55 +9,60 @@ import { ActionType, useCreateReducer } from '@/hooks/useCreateReducer';
 import PlayBar from '@/components/PlayBar';
 import { Howl, Howler } from 'howler';
 import { search } from '@/apis/musicApi';
+import { isEmpty } from '@/utils/common';
 
 interface InitialState {
   searchLoading: boolean;
   playLoading: boolean;
   searchList: IMusicSearchResult[];
-  palyList: IMusicSearchResult[];
+  playList: IMusicSearchResult[];
   currentMusic: IMusicSearchResult;
-  nextMusicList: IMusicSearchResult[];
 }
 
 const initialState: InitialState = {
   searchLoading: false,
   playLoading: false,
   searchList: [],
-  palyList: [],
+  playList: [],
   currentMusic: {} as IMusicSearchResult,
-  nextMusicList: [],
 };
 
-interface ContextProps {
-  state: InitialState;
-  dispatch: Dispatch<ActionType<InitialState>>;
-}
-
-const Context = createContext<ContextProps>(undefined!);
-
+let howler: Howl;
 export default function Home() {
-  const contextValue = useCreateReducer<InitialState>({
-    initialState,
-  });
   const [searchPaging, setSearchPaging] = useState<IPaging>({
     pages: 1,
     count: 20,
   });
-  const {
-    state: {
-      currentMusic,
-      playLoading,
-      nextMusicList,
-      searchLoading,
-      searchList,
-    },
-    dispatch,
-  } = contextValue;
+  const [searchList, setSearchList] = useState<IMusicSearchResult[]>([]);
+  const [playList, setPlayList] = useState<IMusicSearchResult[]>([]);
+  const [currentMusic, setCurrentMusic] = useState<IMusicSearchResult>(
+    {} as IMusicSearchResult
+  );
+  const [searchLoading, setSearchLoading] = useState<boolean>(false);
+  const [playLoading, setPlayLoading] = useState<boolean>(false);
+
+  function nextMusic() {
+    const index = playList.findIndex((x) => x.id === currentMusic.id);
+    if (index > 0 && index < playList.length) {
+      setCurrentMusic(playList[index + 1]);
+    } else {
+      howler.pause();
+    }
+  }
+
+  function prevMusic() {
+    const index = playList.findIndex((x) => x.id === currentMusic.id);
+    if (index != 0) {
+      setCurrentMusic(playList[index - 1]);
+    } else {
+    }
+  }
 
   useEffect(() => {
-    if (playLoading) {
-      let sound = new Howl({
-        src: '',
+    if (!isEmpty(currentMusic)) {
+      howler = new Howl({
+        src: 'files/music?id=' + currentMusic.id,
+        format: ['mp3'],
         html5: true,
         autoplay: true,
         volume: 0.5,
@@ -65,65 +70,69 @@ export default function Home() {
 
       if ('mediaSession' in navigator) {
         navigator.mediaSession.setActionHandler('play', function () {
-          sound.play();
+          howler.play();
         });
         navigator.mediaSession.setActionHandler('pause', function () {
-          sound.pause();
+          howler.pause();
+        });
+        navigator.mediaSession.setActionHandler('previoustrack', function () {
+          prevMusic();
+        });
+        navigator.mediaSession.setActionHandler('nexttrack', function () {
+          nextMusic();
         });
       }
 
-      sound.once('load', () => {
-        sound.play();
+      howler.on('load', () => {
+        howler.play();
       });
 
-      sound.on('end', () => {
-        const index = nextMusicList.findIndex((x) => x.id === currentMusic?.id);
-        if (index > 0 && index < nextMusicList.length) {
-          dispatch({ field: 'currentMusic', value: nextMusicList[index] });
-        } else {
-          sound.pause();
-        }
+      howler.on('end', () => {
+        nextMusic();
       });
     }
-  }, [currentMusic, playLoading]);
+  }, [currentMusic]);
 
   async function handleSearch(value: string) {
-    dispatch({ field: 'searchLoading', value: true });
+    setSearchLoading(true);
     const data = await search({ ...searchPaging, query: value });
-    dispatch({ field: 'searchList', value: data });
-    dispatch({ field: 'searchLoading', value: false });
+    setSearchList(data);
+    setSearchLoading(false);
   }
 
   function handlePlay(musicId: number) {}
   function handlePlayAll() {
-    dispatch({ field: 'playLoading', value: true });
-    dispatch({ field: 'palyList', value: searchList });
-    dispatch({ field: 'nextMusicList', value: searchList });
-    dispatch({ field: 'currentMusic', value: searchList[0] });
-    dispatch({ field: 'playLoading', value: false });
+    setPlayLoading(true);
+    setPlayList(searchList);
+    setCurrentMusic(searchList[0]);
+  }
+
+  function handleNext() {
+    nextMusic();
   }
   function handlePause() {}
 
   return (
     <main className={styles.container}>
-      <Context.Provider value={{ ...contextValue }}>
+      {!isEmpty(currentMusic) && (
         <PlayBar
+          onNext={handleNext}
           title={currentMusic.name}
           description={`${currentMusic.artist} - ${currentMusic.name}`}
         />
-        <SearchBar searching={searchLoading} onSearch={handleSearch} />
-        {searchList.length > 0 && <PlayListAction onPlayAll={handlePlayAll} />}
-        <PlayList>
-          {searchList.map((x) => (
-            <PlayListItem
-              key={x.id}
-              title={x.name}
-              description={`${x.artist} - ${x.name}`}
-              onClickLeft={() => handlePlay(x.id)}
-            />
-          ))}
-        </PlayList>
-      </Context.Provider>
+      )}
+      <SearchBar searching={searchLoading} onSearch={handleSearch} />
+      {searchList.length > 0 && <PlayListAction onPlayAll={handlePlayAll} />}
+      <PlayList>
+        {searchList.map((x) => (
+          <PlayListItem
+            key={x.id}
+            title={x.name}
+            description={`${x.artist} - ${x.name}`}
+            onClickLeft={() => handlePlay(x.id)}
+          />
+        ))}
+      </PlayList>
     </main>
   );
 }
--- nextjs/server.ts
+++ nextjs/server.ts
@@ -1,6 +1,9 @@
 import { createServer } from 'http';
-import { parse } from 'url';
 import next from 'next';
+import path from 'path';
+import * as fs from 'fs';
+import prisma from './prisma/prisma';
+import express from 'express';
 
 const port = parseInt(process.env.PORT || '3000', 10);
 const dev = process.env.NODE_ENV !== 'production';
@@ -8,11 +11,24 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true);
-    handle(req, res, parsedUrl);
-  }).listen(port);
+  const server = express();
+  server.use('/files/music', async (req, res) => {
+    const id = +(req.query['id'] || 0);
+    if (!id) res.status(404).end('File not found');
+    const musicSavePath = process.env.MUSIC_SAVE_PATH!;
+    const music = await prisma.music.findUnique({ where: { id } });
+    const filePath = path.resolve(musicSavePath, music?.url!);
 
+    if (await fs.existsSync(filePath)) {
+      res.sendFile(filePath);
+    } else {
+      res.status(404).send('File not found');
+    }
+  });
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+  createServer(server).listen(port);
   console.log(
     `> Server listening at http://localhost:${port} as ${
       dev ? 'development' : process.env.NODE_ENV
