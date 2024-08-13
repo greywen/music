11e1c2227520b360fe61b2a54f4122c23db9016a
gray <gray.wen@outlook.com>
2024-08-13 16:21:11 +0800

    fix pwa error

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -13,6 +13,14 @@ export default function RootLayout({
           name='viewport'
           content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
         ></meta>
+
+        <link
+          rel='manifest'
+          href='/manifest.json'
+          crossOrigin='use-credentials'
+        />
+        <link rel='apple-touch-icon' href='/icon.png' />
+        <link rel='shortcut icon' href='/icon.png' />
       </head>
       <body>
         {children}
--- nextjs/next.config.mjs
+++ nextjs/next.config.mjs
@@ -1,5 +1,6 @@
 import nextPWA from 'next-pwa';
 
+console.log(process.env?.NODE_ENV);
 const isDev = process.env?.NODE_ENV === 'development';
 const url = new URL(process.env?.API_URL);
 
