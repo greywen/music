58408d32975590211f3399bfcd41e0e14bd9c96f
gray <gray.wen@outlook.com>
2024-08-15 08:59:07 +0800

    change next config

--- nextjs/next.config.mjs
+++ nextjs/next.config.mjs
@@ -2,7 +2,6 @@ import nextPWA from 'next-pwa';
 
 console.log(process.env?.NODE_ENV);
 const isDev = process.env?.NODE_ENV === 'development';
-const url = new URL(process.env?.API_URL);
 
 const withPWA = nextPWA({
     dest: 'public',
@@ -13,7 +12,7 @@ const withPWA = nextPWA({
 
 const nextConfig = {
     images: {
-        domains: [url.hostname],
+        domains: ["127.0.0.1", "doubleu.work"],
     },
 };
 
