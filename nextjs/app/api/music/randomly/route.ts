ca9a9318520407a185c1beee7cea4cd864dc7ab7
gy <gray.wen@outlook.com>
2024-09-03 19:59:21 +0800

    adjust route

--- nextjs/app/api/music/randomly/route.ts
+++ nextjs/app/api/music/randomly/route.ts
@@ -31,7 +31,7 @@ const search = async () => {
   );
 };
 
-export async function GET() {
+export async function GET(request: Request) {
   const data = await search();
   return new Response(JSON.stringify(data));
 }
