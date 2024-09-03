2b80c6ef93df6bf0cc39306149202dcefa1eb375
gy <gray.wen@outlook.com>
2024-09-03 20:25:09 +0800

    adjust route

--- nextjs/app/api/music/fuck/route.ts
+++ nextjs/app/api/music/fuck/route.ts
@@ -9,28 +9,28 @@ interface IRandomResult {
   coverId: number;
 }
 
+const random100 = async () => {
+  return await prisma.$queryRaw<IRandomResult[]>(
+    Prisma.sql`
+        SELECT DISTINCT ON
+          ( M."id" ) M."id",
+          M."name" AS "name",
+          S."name" AS singer,
+          AL."name" AS album,
+          AL."coverId" AS "coverId" 
+        FROM
+          "Music"M 
+          LEFT JOIN "MusicArtist" MA ON M.ID = MA."musicId"
+          LEFT JOIN "Singer" S ON ma."singerId" = S."id"
+          LEFT JOIN "Album" AL ON M."albumId" = AL."id" 
+        WHERE
+          M."id" IN ( SELECT M."id" FROM "Music" M ORDER BY RANDOM( ) LIMIT 100 ) 
+        ORDER BY
+          M."id",
+          M."createdAt" DESC;`
+  );
+};
 export async function GET() {
-  const random100 = async () => {
-    return await prisma.$queryRaw<IRandomResult[]>(
-      Prisma.sql`
-  SELECT DISTINCT ON
-    ( M."id" ) M."id",
-    M."name" AS "name",
-    S."name" AS singer,
-    AL."name" AS album,
-    AL."coverId" AS "coverId" 
-  FROM
-    "Music"M 
-    TABLESAMPLE SYSTEM (10)
-    LEFT JOIN "MusicArtist" MA ON M.ID = MA."musicId"
-    LEFT JOIN "Singer" S ON ma."singerId" = S."id"
-    LEFT JOIN "Album" AL ON M."albumId" = AL."id" 
-  ORDER BY
-    M."id",
-    M."createdAt" DESC
-  LIMIT 100;`
-    );
-  };
   const data = await random100();
   return new Response(JSON.stringify(data));
 }
