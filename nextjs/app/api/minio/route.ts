949ae6f61b4e8af84c985be1149ab14c5e301eac
gy <gray.wen@outlook.com>
2024-09-03 20:57:33 +0800

    fixed music file is not found

--- nextjs/app/api/minio/route.ts
+++ nextjs/app/api/minio/route.ts
@@ -1,14 +1,25 @@
 import prisma from '@/prisma/prisma';
+import { existsSync } from 'fs';
 import { Client } from 'minio';
 import * as path from 'path';
 
 async function processUpload(
   minioClient: Client,
-  musics: { id: number; url: string }[]
+  musics: { id: number; url: string; lyricId: number | null }[]
 ) {
   const promises = musics.map(async (music) => {
     const { MINIO_BUCKET_NAME } = process.env!;
     const filePath = process.env.MUSIC_SAVE_PATH! + `/${music.url}`;
+
+    const existFile = await existsSync(filePath);
+    if (!existFile) {
+      await prisma.musicArtist.deleteMany({ where: { musicId: music.id } });
+      await prisma.music.delete({ where: { id: music.id } });
+      music.lyricId &&
+        (await prisma.lyric.delete({ where: { id: music.lyricId } }));
+      return;
+    }
+
     const fileName = path.basename(filePath);
     const objectName = 'data/' + fileName;
     await minioClient.fPutObject(MINIO_BUCKET_NAME!, objectName, filePath, {
@@ -36,7 +47,7 @@ export async function POST() {
   });
 
   const musics = await prisma.music.findMany({
-    select: { id: true, url: true },
+    select: { id: true, url: true, lyricId: true },
     where: { objectName: { equals: null } },
   });
 
