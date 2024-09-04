68d329f51bfb8b8924bab279147d785228bfe5b3
gray <gray.wen@outlook.com>
2024-09-04 09:23:52 +0800

    update uploadToMinIO

--- nextjs/app/api/minio/route.ts
+++ nextjs/app/api/minio/route.ts
@@ -3,15 +3,30 @@ import { getFileNameAndExtension } from '@/utils/common';
 import { existsSync } from 'fs';
 import { Client } from 'minio';
 
-async function uploadToMinIO(
-  minIOClient: Client,
-  musics: { id: number; url: string; lyricId: number | null }[]
-) {
+export async function POST() {
+  const { MINIO_ACCESS_KEY, MINIO_SECRET_KEY, MINIO_ENDPOINT } = process.env;
+
+  const minIOClient = new Client({
+    endPoint: MINIO_ENDPOINT!,
+    useSSL: true,
+    accessKey: MINIO_ACCESS_KEY!,
+    secretKey: MINIO_SECRET_KEY!,
+  });
+
+  const musics = await prisma.music.findMany({
+    select: { id: true, url: true, lyricId: true },
+    where: { objectName: { equals: null } },
+    orderBy: { id: 'asc' },
+    take: 50,
+  });
+
+  console.log('musics count:', musics.length);
+
   const { MINIO_BUCKET_NAME, MUSIC_SAVE_PATH } = process.env!;
-  const promises = musics.map(async (music) => {
+  for (const music of musics) {
     try {
       const filePath = MUSIC_SAVE_PATH! + `/${music.url}`;
-      
+
       const existFile = await existsSync(filePath);
       if (!existFile) {
         await prisma.musicArtist.deleteMany({ where: { musicId: music.id } });
@@ -34,31 +49,6 @@ async function uploadToMinIO(
     } catch (error) {
       console.error(`Error processing music ID ${music.id}:`, error);
     }
-  });
-
-  const promiseList = await Promise.all(promises);
-  return promiseList;
-}
-
-export async function POST() {
-  const { MINIO_ACCESS_KEY, MINIO_SECRET_KEY, MINIO_ENDPOINT } = process.env;
-
-  const minIOClient = new Client({
-    endPoint: MINIO_ENDPOINT!,
-    useSSL: true,
-    accessKey: MINIO_ACCESS_KEY!,
-    secretKey: MINIO_SECRET_KEY!,
-  });
-
-  const musics = await prisma.music.findMany({
-    select: { id: true, url: true, lyricId: true },
-    where: { objectName: { equals: null } },
-    orderBy: { id: 'asc' },
-  });
-
-  console.log('musics count:', musics.length);
-
-  await uploadToMinIO(minIOClient, musics);
-
+  }
   return Response.json({ message: 'ok' });
 }
