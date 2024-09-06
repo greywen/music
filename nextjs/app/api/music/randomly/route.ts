9fd71afd08b1dabbf469d4904e24556981c43a7f
gy <gray.wen@outlook.com>
2024-09-06 20:59:57 +0800

    adjust random sql

--- nextjs/app/api/music/randomly/route.ts
+++ nextjs/app/api/music/randomly/route.ts
@@ -12,23 +12,29 @@ interface IRandomResult {
 
 const search = async () => {
   return await prisma.$queryRaw<IRandomResult[]>`
-    SELECT DISTINCT ON
-      ( M."id" ) M."id",
-      M."name" AS "name",
-      S."name" AS "artist",
-      AL."name" AS album,
-      C."filePath" AS "coverPath" 
-    FROM
-      "Music" M 
-      LEFT JOIN "MusicArtist" MA ON M.ID = MA."musicId"
-      LEFT JOIN "Singer" S ON ma."singerId" = S."id"
-      LEFT JOIN "Album" AL ON M."albumId" = AL."id" 
-      LEFT JOIN "Cover" C ON C."id" = AL."coverId" 
-    WHERE
-      M."id" IN ( SELECT M."id" FROM "Music" M ORDER BY RANDOM() LIMIT 100 ) 
-    ORDER BY
-      M."id",
-      M."createdAt" DESC;`;
+  WITH AllArtists AS (
+		SELECT
+			MA."musicId",
+			STRING_AGG ( S."name", ', ' ) AS artist 
+		FROM
+			"MusicArtist" MA
+			JOIN "Singer" S ON MA."singerId" = S."id" 
+		GROUP BY
+			MA."musicId" 
+		) SELECT M
+		."id",
+		M."name" AS "name",
+		AA.artist,
+		AL."name" AS album,
+		C."filePath" as "coverPath"
+	FROM
+		"Music" M 
+		LEFT JOIN AllArtists AA ON M."id" = AA."musicId"
+		LEFT JOIN "Album" AL ON M."albumId" = AL."id"
+    LEFT JOIN "Cover" C ON C."id" = AL."coverId" 
+	WHERE
+      M."id" IN ( SELECT M."id" FROM "Music" M TABLESAMPLE SYSTEM (10) LIMIT 100 ) 
+	GROUP BY M."id", M."name", AA.artist, AL."name", C."filePath";`;
 };
 
 export async function GET() {
