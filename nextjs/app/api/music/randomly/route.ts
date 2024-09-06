660977937988690b5b97b280ff8558bede2c9050
gy <gray.wen@outlook.com>
2024-09-06 21:04:34 +0800

    adjust random sql

--- nextjs/app/api/music/randomly/route.ts
+++ nextjs/app/api/music/randomly/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/prisma/prisma';
 import { getFilePublicUrl } from '@/utils/minio';
+import { Prisma } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
 interface IRandomResult {
@@ -11,30 +12,31 @@ interface IRandomResult {
 }
 
 const search = async () => {
-  return await prisma.$queryRaw<IRandomResult[]>`
-  WITH AllArtists AS (
-		SELECT
-			MA."musicId",
-			STRING_AGG ( S."name", ', ' ) AS artist 
-		FROM
-			"MusicArtist" MA
-			JOIN "Singer" S ON MA."singerId" = S."id" 
-		GROUP BY
-			MA."musicId" 
-		) SELECT M
-		."id",
-		M."name" AS "name",
-		AA.artist,
-		AL."name" AS album,
-		C."filePath" as "coverPath"
-	FROM
-		"Music" M 
-		LEFT JOIN AllArtists AA ON M."id" = AA."musicId"
-		LEFT JOIN "Album" AL ON M."albumId" = AL."id"
-    LEFT JOIN "Cover" C ON C."id" = AL."coverId" 
-	WHERE
-      M."id" IN ( SELECT M."id" FROM "Music" M TABLESAMPLE SYSTEM (10) LIMIT 100 ) 
-	GROUP BY M."id", M."name", AA.artist, AL."name", C."filePath";`;
+  return await prisma.$queryRaw<IRandomResult[]>(
+    Prisma.sql`WITH AllArtists AS (
+	    SELECT
+	    		MA."musicId",
+	    		STRING_AGG ( S."name", ', ' ) AS artist 
+	    	FROM
+	    		"MusicArtist" MA
+	    		JOIN "Singer" S ON MA."singerId" = S."id" 
+	    	GROUP BY
+	    		MA."musicId" 
+	    	) SELECT M
+	    	."id",
+	    	M."name" AS "name",
+	    	AA.artist,
+	    	AL."name" AS album,
+	    	C."filePath" as "coverPath"
+	    FROM
+	    	"Music" M 
+	    	LEFT JOIN AllArtists AA ON M."id" = AA."musicId"
+	    	LEFT JOIN "Album" AL ON M."albumId" = AL."id"
+        LEFT JOIN "Cover" C ON C."id" = AL."coverId" 
+	    WHERE
+          M."id" IN ( SELECT M."id" FROM "Music" M TABLESAMPLE SYSTEM (10) LIMIT 100 ) 
+	    GROUP BY M."id", M."name", AA.artist, AL."name", C."filePath";`
+  );
 };
 
 export async function GET() {
