97553c5bf999d7f9debfc20a7444597ecb7fa05b
gy <gray.wen@outlook.com>
2024-09-06 10:28:22 +0800

    fix build error

--- nextjs/utils/minioClient.ts
+++ nextjs/utils/minioClient.ts
@@ -1,16 +1,18 @@
 import { Client } from 'minio';
 
-const { MINIO_ACCESS_KEY, MINIO_SECRET_KEY, MINIO_ENDPOINT } = process.env;
+class minIOClient {
+  client: Client;
+  constructor() {
+    const { MINIO_ACCESS_KEY, MINIO_SECRET_KEY, MINIO_ENDPOINT } = process.env;
+    this.client = new Client({
+      endPoint: MINIO_ENDPOINT!,
+      useSSL: true,
+      accessKey: MINIO_ACCESS_KEY!,
+      secretKey: MINIO_SECRET_KEY!,
+    });
+  }
 
-export default class minIOClient {
-  static client = new Client({
-    endPoint: MINIO_ENDPOINT!,
-    useSSL: true,
-    accessKey: MINIO_ACCESS_KEY!,
-    secretKey: MINIO_SECRET_KEY!,
-  });
-
-  static presignedGetObject = async (
+  presignedGetObject = async (
     bucketName: string,
     objectName: string,
     expires?: number,
@@ -26,3 +28,5 @@ export default class minIOClient {
     );
   };
 }
+
+export default new minIOClient();
